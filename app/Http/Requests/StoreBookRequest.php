5df891c9c5d239207082ad1b0dd3945ee532ec93
Rabie-s <asdfw.jsd@gmail.com>
2024-05-15 14:25:30 +0300

    Add validation request to Book

---
+++ app/Http/Requests/StoreBookRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreBookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string',
+            'image_path' => 'required|image|mimes:jpg,png,jpeg',
+            'status' => 'integer',
+            'description' => 'string',
+            'active' => 'boolean',
+            'done' => 'boolean',
+        ];
+    }
+}
